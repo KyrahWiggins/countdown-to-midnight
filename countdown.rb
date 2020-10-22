73cd336667b9fdb9957595c3b51b4aa07f703f8d
KyrahWiggins <kyrah.wiggins@gmail.com>
2020-10-22 16:20:26 -0400

    commit tree ced909b76551139d7a9d7c9b705b9aa73de2c33d

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
-end
+def countdown(y)
+    while y > 0 
+        puts "#{y} SECOND(S)!"
+        y -= 1
+    end
+    return "HAPPY NEW YEAR!"
+	 end
+
+
+ def countdown_with_sleep(y)
+    while y > 0 
+        puts "#{y} SECOND(S)!"
+        sleep 1
+        y -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end 
